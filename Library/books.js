33d96b0f82d87b824ee9a865048cae785839b9e2
Sajid Mohammad <sajidmoha03@gmail.com>
2022-07-02 13:06:51 -0700

    set basic page layout and grid area

--- Library/books.js
+++ Library/books.js
@@ -1,18 +1,23 @@
+let myLibrary = [];
+
 function Book(title, author, numPages, read) {
     this.title = title;
     this.author = author;
     this.pages = numPages;
     this.read = read;
-    // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-    this.info = function () {
-        if (this.read) {
-            return `${this.title} by ${this.author}, ${this.pages} pages, read`;
-        } else {
-            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
-        }
-    };
 }
 
-const GEH = new Book('Green Eggs & Ham' , 'Dr. Seuss', 10, true);
-console.log(GEH);
-console.log(GEH.info());
+// "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+Book.prototype.info = function () {
+    if (this.read) {
+        return `${this.title} by ${this.author}, ${this.pages} pages, read`;
+    } else {
+        return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+    }
+};
+
+function addBookToLibrary(title, author, numPages, read) {
+    let book = new Book(title, author, numPages, read);
+    myLibrary.push(book);
+}
+
