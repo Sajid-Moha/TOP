40faf4d87468d6f3349a0cacb16434f669afb03b
Sajid Mohammad <sajidmoha03@gmail.com>
2022-07-03 18:52:33 -0700

    implemented unstyled modal

--- Library/modal.js
+++ Library/modal.js
@@ -1,5 +1,11 @@
+const modal = document.querySelector('.modal');
+
 const add_book = document.querySelector('.add');
 add_book.addEventListener('click', () => {
-    const modal = document.createElement('div');
-    
+    modal.style.display = 'block';
 });
+
+const close_modal = document.querySelector('#close');
+close_modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
