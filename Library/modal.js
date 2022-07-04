c5687a58aede31feae398d4a14ecae2b8f2b983a
Sajid Mohammad <sajidmoha03@gmail.com>
2022-07-03 19:23:53 -0700

    added media query to stack content at width of 950px

--- Library/modal.js
+++ Library/modal.js
@@ -1,11 +1,14 @@
+const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 
 const add_book = document.querySelector('.add');
 add_book.addEventListener('click', () => {
     modal.style.display = 'block';
+    body.style.position = 'fixed';
 });
 
 const close_modal = document.querySelector('#close');
 close_modal.addEventListener('click', () => {
     modal.style.display = 'none';
+    body.style.position = 'static';
 })
